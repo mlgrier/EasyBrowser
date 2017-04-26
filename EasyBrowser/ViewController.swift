59ad07dcbd6ffbafe699034f80d97b8da27a6f32
Marco Grier <mlgrier@gmail.com>
2017-04-25 20:59:03 -0500

    Basic code completed to open web page

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
+    }
+    
+    func openTapped() {
+        
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        //If using in a ipad I will need this line of code below
+        //ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction!) {
+        
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        title = webView.title
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +66,10 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
+
+
 
